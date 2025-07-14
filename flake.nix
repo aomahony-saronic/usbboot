d02bddfdd9af9239db7ddf100c86b226dd7695ac
Andrew O'Mahony <andrew.omahony@saronic.com>
2025-07-14 14:44:07 -0500

    Flake file for compilation and inclusion in our Nix code

--- flake.nix
+++ flake.nix
@@ -12,11 +12,51 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "usbboot";
+
+          nativeBuildInputs = with pkgs; [
+            gcc
+            gnumake
+            pkg-config
+            libusb1
+            xxd
+            tree
+          ];
+
+          src = ./.;
+
+          buildPhase = ''
+            # Run our make program
+            INSTALL_PREFIX=$out make
+          '';
+          installPhase = ''
+            # Make our output directory
+            mkdir -p $out/bin
+            mkdir -p $out/share/rpiboot/mass-storage-gadget64
+
+            # Copy our binary to the expected directory
+            cp rpiboot $out/bin/
+
+            # Copy our bootloader to the expected directory 
+            # !!! I'm not sure why this is being copied over, as it isn't used unless we dd it
+            # !!! ourselves onto a USB flash drive.  This program uses bootfiles.bin and either uploads
+            # !!! it to the Pi directly, or acts as a file server for the Pi bootloader to request from
+
+	          cp mass-storage-gadget64/boot.img $out/share/rpiboot/mass-storage-gadget64/
+            cp mass-storage-gadget64/config.txt $out/share/rpiboot/mass-storage-gadget64/
+	          cp mass-storage-gadget64/bootfiles.bin $out/share/rpiboot/mass-storage-gadget64/
+
+            # Make sure we copied everything
+            tree $out
+          '';
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             # Build dependencies
             gcc
             gnumake
+            pkg-config
             libusb1
             
             # Development tools
@@ -29,7 +69,7 @@
           shellHook = ''
             echo "rpiboot development environment"
             echo "Generating compile_commands.json..."
-            bear make
+            bear -- make
           '';
         };
       });
